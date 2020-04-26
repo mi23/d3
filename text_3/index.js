b65f081bcf1ec4649b0ef19055790a87ac36f103
mi23 <mirela2309@yahoo.co.uk>
2020-04-26 19:31:54 +0100

    group and transform

--- text_3/index.js
+++ text_3/index.js
@@ -4,22 +4,24 @@ const svg = canvas.append('svg')
 svg.attr('height', 600)
 svg.attr('width', 600);
 
+const group = svg.append('g')
+    .attr('transform', 'translate(0,300)');
 
 //append shapes tosvg container
-svg.append('rect')
+group.append('rect')
     .attr('width', 200)
     .attr('height', 100)
     .attr('fill', '#47694f')
     .attr('x', 100)
     .attr('y',100);
 
-svg.append('circle')
+group.append('circle')
 .attr('r', 50)
 .attr('cx', 300)
 .attr('cy', 70)
 .attr('fill', 'pink');
 
-svg.append('line')
+group.append('line')
 .attr('x1', 370)
 .attr('y1', 20)
 .attr('x2', 400)
@@ -30,5 +32,5 @@ svg.append('text')
 .attr('x', 150)
 .attr('y', 400)
 .attr('fill','grey')
-.text('Hello D3!')
+.text('hello D3!')
 .style('font-family', 'arial');
