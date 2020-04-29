10a3cad8d67fbc2a32c84d50f51d9176d0420802
mi23 <mirela2309@yahoo.co.uk>
2020-04-29 15:47:58 +0100

    Enter selection adding extra shapes dependent on data.

--- -8/index.js
+++ -8/index.js
@@ -1,4 +1,7 @@
 const data= [
+
+    {width: 600, height: 300, fill: 'pink'},
+    {width: 400, height: 200, fill: 'blue'},
     {width: 200, height: 100, fill: 'pink'},
     {width: 100, height: 60, fill: 'orange'},
     {width: 60, height: 30, fill: 'yellow'}
@@ -7,8 +10,16 @@ const data= [
 
 const svg = d3.select('svg');
 
-const rect = svg.selectAll('rect')
+//add attr to rects in the DOM
+const rects = svg.selectAll('rect')
 .data(data )
+.attr('width', (d, i, n)=> d.width)
+.attr('height', d => d.height)
+.attr('fill', d => d.fill);
+
+//append the enter selection to DOM
+rects.enter()
+    .append('rect')
 //on the same line but few parameters
 .attr('width', (d, i, n)=> d.width)
 //on the same line but one parameter
