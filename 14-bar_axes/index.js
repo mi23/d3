9d1e392a39a8ad1222e2dfc1f9c5d12c8c17d10a
mi23 <mirela2309@yahoo.co.uk>
2020-05-03 00:31:02 +0100

    axes

--- 14-bar_axes/index.js
+++ 14-bar_axes/index.js
@@ -4,7 +4,7 @@ const svg = d3.select('.canvas')
 .attr('width', 600)
 .attr('height', 600);
 //create margins and dimensions
-const margin = { top: 20, right: 20, bottom: 100, left: 100}
+const margin = { top: 20, right: 50, bottom: 100, left: 100}
 const graphWidth = 600 - margin.left-margin.right;
 const graphHeight= 600 - margin.top - margin.bottom;
 
@@ -13,13 +13,24 @@ const graph = svg.append('g')
 .attr('height', graphHeight)
 .attr('transform', `translate(${margin.left},${margin.top})`)
 
+const xAxisGroup =graph.append('g')
+//axis going down on the page
+.attr('transform', `translate(0, ${graphHeight}) `);
+const yAxisGroup =graph.append('g')
 
 d3.json('menu.json').then(data => {
 
 
 const y =d3.scaleLinear()
 .domain([0,d3.max  (data, d => d.orders)])
-.range([0,500]);
+.range([0,graphHeight]);
+
+const x = d3.scaleBand()
+.domain(data.map(item=>item.name))
+.range([0,graphWidth])
+.padding([0.2]);
+//.paddingInner(0.2)
+//.paddingOuter(0.2);
 
 // const max = d3.max  (data, d => d.orders);
 // const min = d3.min (data, d => d.orders);
@@ -46,5 +57,12 @@ rects.enter()
 .attr('fill', 'orange')
 .attr('x', (d,i) => i*70);
 
+// create and call axes. at the bottom as dependednt on data
+const xAxis = d3.axisBottom(x)
+
+const yAxis = d3.axisLeft(y);
+
+xAxisGroup.call(xAxis);
+yAxisGroup.call(yAxis);
 
 })
