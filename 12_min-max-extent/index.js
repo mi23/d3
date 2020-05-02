66ccc1892c622c421b9faae5f0615c2fe27fd1e6
mi23 <mirela2309@yahoo.co.uk>
2020-05-02 17:24:20 +0100

    margins

--- 12_min-max-extent/index.js
+++ 12_min-max-extent/index.js
@@ -3,15 +3,18 @@ const svg = d3.select('svg');
 
 d3.json('menu.json').then(data => {
 
+
 const y =d3.scaleLinear()
-.domain([0,1000])
+.domain([0,d3.max  (data, d => d.orders)])
 .range([0,500]);
 
+// const max = d3.max  (data, d => d.orders);
+// const min = d3.min (data, d => d.orders);
+// const extent = d3.extent (data, d => d.orders);
 
+// console.log(max, min)
+// console.log(extent)
 
-console.log(y(400))
-console.log(y(0))
-console.log(y(900))
 
 //join the data to rects
 const rects = svg.selectAll('rect')
---
+++ 13_bar-chart/index.js
@@ -0,0 +1,49 @@
+//select svg container first
+const svg = d3.select('.canvas')
+.append('svg')
+.attr('width', 600)
+.attr('height', 600);
+//create margins and dimensions
+const margin = { top: 20, right: 20, bottom: 100, left: 100}
+const graphWidth = 600 - margin.left-margin.right;
+const graphHeight= 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+.attr('width'. graphWidth)
+.attr('height', graphHeight)
+.attr('transform', 'translate(${margin.left},${margin.top})')
+
+d3.json('menu.json').then(data => {
+
+
+const y =d3.scaleLinear()
+.domain([0,d3.max  (data, d => d.orders)])
+.range([0,500]);
+
+// const max = d3.max  (data, d => d.orders);
+// const min = d3.min (data, d => d.orders);
+// const extent = d3.extent (data, d => d.orders);
+
+// console.log(max, min)
+// console.log(extent)
+
+
+//join the data to rects
+const rects = graphWidth.selectAll('rect')
+.data(data)
+
+//give attributes to the first element in dom
+rects.attr('width', 50)
+.attr('height', d=> y(d.orders))
+.attr('fill', 'orange')
+.attr('x', (d,i) => i*70);
+
+rects.enter()
+.append('rect')
+.attr('width', 50)
+.attr('height', d=> y(d.orders))
+.attr('fill', 'orange')
+.attr('x', (d,i) => i*70);
+
+
+})
