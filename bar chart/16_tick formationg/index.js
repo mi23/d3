4740fb7b5c0b56893185dc55a191942ef5fc9601
mi23 <mirela2309@yahoo.co.uk>
2020-05-08 19:33:56 +0100

    finished firebase

--- bar chart/16_tick formationg/index.js
+++ bar chart/16_tick formationg/index.js
@@ -21,60 +21,65 @@ const yAxisGroup =graph.append('g')
 //new data source. havew replaced up to .then
 db.collection('dishes').get().then((res) => {
 
-    console.log(res)
+var data = [];
+res.docs.forEach(doc => {
+    data.push(doc.data())
+})
 
-// const y =d3.scaleLinear()
-// .domain([0,d3.max  (data, d => d.orders)])
-// .range([graphHeight, 0]);
+console.log(data)
 
-// const x = d3.scaleBand()
-// .domain(data.map(item=>item.name))
-// .range([0,graphWidth])
-// .padding([0.2]);
-// //.paddingInner(0.2)
-// //.paddingOuter(0.2);
+const y =d3.scaleLinear()
+.domain([0,d3.max  (data, d => d.orders)])
+.range([graphHeight, 0]);
 
-// // const max = d3.max  (data, d => d.orders);
-// // const min = d3.min (data, d => d.orders);
-// // const extent = d3.extent (data, d => d.orders);
+const x = d3.scaleBand()
+.domain(data.map(item=>item.name))
+.range([0,graphWidth])
+.padding([0.2]);
+//.paddingInner(0.2)
+//.paddingOuter(0.2);
 
-// // console.log(max, min)
-// // console.log(extent)
+// const max = d3.max  (data, d => d.orders);
+// const min = d3.min (data, d => d.orders);
+// const extent = d3.extent (data, d => d.orders);
+
+// console.log(max, min)
+// console.log(extent)
 
 
-// //join the data to rects
-// const rects = graph.selectAll('rect')
-// .data(data)
+//join the data to rects
+const rects = graph.selectAll('rect')
+.data(data)
 
-// //give attributes to the first element in dom
-// rects.attr('width', x.bandwidth)
-// .attr('height', d=> graphHeight - y(d.orders))
-// .attr('fill', 'orange')
-// .attr('x', d => x(d.name))
-// .attr('y', d=> y(d.orders));
+//give attributes to the first element in dom
+rects.attr('width', x.bandwidth)
+.attr('height', d=> graphHeight - y(d.orders))
+.attr('fill', 'orange')
+.attr('x', d => x(d.name))
+.attr('y', d=> y(d.orders));
 
-// rects.enter()
-// .append('rect')
-// .attr('width', x.bandwidth)
-// .attr('height', d=> graphHeight- y(d.orders))
-// .attr('fill', 'orange')
-// .attr('x', d => x(d.name))
-// .attr('y', d=> y(d.orders));
+rects.enter()
+.append('rect')
+.attr('width', x.bandwidth)
+.attr('height', d=> graphHeight- y(d.orders))
+.attr('fill', 'orange')
+.attr('x', d => x(d.name))
+.attr('y', d=> y(d.orders));
 
-// // create and call axes. at the bottom as dependednt on data
-// const xAxis = d3.axisBottom(x)
+// create and call axes. at the bottom as dependednt on data
+const xAxis = d3.axisBottom(x)
 
-// const yAxis = d3.axisLeft(y)
-// .ticks(5)
-// .tickFormat(d => d + ' orders');
+const yAxis = d3.axisLeft(y)
+.ticks(5)
+.tickFormat(d => d + ' orders');
 
-// xAxisGroup.call(xAxis);
-// yAxisGroup.call(yAxis);
+xAxisGroup.call(xAxis);
+yAxisGroup.call(yAxis);
 
-// xAxisGroup.selectAll('text')
-//     .attr('transform', 'rotate(-40)')
-//     .attr('text-anchor', 'end')
-//     .attr('fill', 'green' )
+xAxisGroup.selectAll('text')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end')
+    .attr('fill', 'green' )
 
 
 
